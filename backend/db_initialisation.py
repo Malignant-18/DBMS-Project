b5bad386b681e6cfdb53444866caa9166f136d33
anujith <anujithanil.2005@gmail.com>
2025-09-20 16:46:01 +0530

    mysqlite

---
+++ backend/db_initialisation.py
@@ -0,0 +1,9 @@
+import sqlite3
+import os
+script_dir = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(script_dir, "Voting_System.db")
+mycon = sqlite3.connect(db_path)
+mycur=mycon.cursor()
+mycur.execute("CREATE TABLE IF NOT EXISTS Users(reg_no char(10)  primary key,password text not null,name varchar(25) not null)")
+mycon.commit()
+mycon.close()
