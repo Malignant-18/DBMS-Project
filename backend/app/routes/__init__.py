f0873616397236f1737704604d8787d3173e3dcf
Joe George <joeofficial215@gmail.com>
2025-09-21 20:19:15 +0530

    member user routes

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -1,7 +1,9 @@
 from .auth_routes import auth_bp
 from .club_routes import club_bp
+from .user_routes import user_bp
 
 def register_routes(app):
     app.register_blueprint(auth_bp)
     app.register_blueprint(club_bp)
+    app.register_blueprint(user_bp)
     # Later, you can register vote_bp or other blueprints here
