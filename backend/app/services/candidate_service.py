023cba5aa33e04a230454442a684a56e6fdcc5b5
anujith <anujithanil.2005@gmail.com>
2025-09-27 14:44:53 +0530

    Revert "voting logic"
    This reverts commit dc2d1e323617d98676f49772bf188aeaf230d1c1.

--- backend/app/services/candidate_service.py
+++
@@ -1,4 +0,0 @@
-from ..models import candidate_model
-
-def increement_vote(candidate_id):
-    candidate_model.increase_vote(candidate_id)
--- backend/app/services/votes_service.py
+++
@@ -1,7 +0,0 @@
-from ..models import votes_model
-
-def add_voting_record(reg_no,election_id):
-    return votes_model.add_vote_record(reg_no,election_id)
-
-def check_already_voted(reg_no,election_id):
-    return votes_model.check_vote(reg_no,election_id)
